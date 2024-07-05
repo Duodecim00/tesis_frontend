b7338fcb94df34ae10809a633f131f8e77e36662
Duodecim00 <josem2740@gmail.com>
2024-07-04 21:39:25 -0400

    first commit

---
+++ frontent_tesis/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register.jsx';
+import './App.css';
+
+function App() {
+ 
+
+  return (
+ 
+    <Router>
+      <Routes>
+        <Route path="/test" element={<Register/>} />
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App
---
+++ frontent_tesis/src/components/Forminput.jsx
@@ -0,0 +1,13 @@
+
+
+function Forminput() {
+
+  return (
+  
+        <input type="text" placeholder="patata" />
+   
+   
+  )
+}
+
+export default Forminput;
