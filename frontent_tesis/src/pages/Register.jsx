382d8715f9c041c3f16bcca4613a1c96649208d8
Duodecim00 <josem2740@gmail.com>
2024-07-13 18:33:44 -0400

    esqueleto de pagina login

--- frontent_tesis/src/pages/Register.jsx
+++ frontent_tesis/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-
+import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import theme from '../color/color';
 import Typography from '@mui/material/Typography';
@@ -17,42 +17,16 @@ function Register() {
   const [grado,setGrado] = useState("")
   const [seccion,setSeccion] = useState("")
 
- async function probando() {
-    const url = "http://localhost:3000/newstudent";
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        nombrecompleto:`${nombrecompleto}`,
-        cedula:`${cedula}`,
-        grado:`${grado}`,
-        seccion:`${seccion}`,
-      })}
-  try {
-    const response = await fetch(url,requestOptions);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.error(error.message);
-  }
-  }
-
   return(
 
-<Box sx={{ width: '650px', backgroundColor: theme.palette.primary.light, margin: '50%', borderRadius: 1}}alignItems="center">
+<Box sx={{ width: '650px', backgroundColor: theme.palette.primary.light, marginLeft: 'auto',marginRight: 'auto', marginTop: '150px', borderRadius: 1}}alignItems="center">
 <div style={{textAlign: 'center'}}>
   <h1 style={{color: theme.palette.primary.dark }}>register student</h1>
 </div>
     
 
-    <Grid container spacing={2} sx={{ padding: '15px'}}>
+    <Grid container spacing={2} sx={{ padding: '15px',}}>
+      
         <Grid item xs={12} >
             <Typography sx={{color: theme.palette.primary.dark}} variant="subtitle1" component="div">Studen Name</Typography>  
               <Grid container spacing={1}>
@@ -87,6 +61,8 @@ function Register() {
 </Grid>
     
 </Box>
+
+
 
     
 
