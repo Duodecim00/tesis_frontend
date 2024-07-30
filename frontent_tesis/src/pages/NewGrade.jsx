8ca549e0c0cce7ba295d4af8de5dfcc290ff5d13
Fernando Parra <thefer767@gmail.com>
2024-07-30 14:51:26 -0400

    NewGrade Changes

--- frontent_tesis/src/pages/NewGrade.jsx
+++ frontent_tesis/src/pages/NewGrade.jsx
@@ -1,4 +1,3 @@
-
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { useState } from 'react';
@@ -14,51 +13,47 @@ import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import './register.css';
-
-
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { colors } from '@mui/material';
 
 function NewGrade() {
- /* async function probando() {
-    const url = "http://localhost:3000/newstudent";
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+
+  const [Name,setName] = useState("");
+
+  const [Teacher,setTeacher] = useState("");
+  const [Seccion,setSeccion] = useState("");
+  
+  const [Weeks,setWeeks] = useState("");
+  const [startDate,setstartDate] = useState();
+
+  const [TimeStart,setTimeStart] = useState();
+  const [timeEnd,setTimeEnd] = useState();
+
+  const [days, setdays] = useState([]);
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
       },
-      body: JSON.stringify({
-        nombrecompleto:${nombrecompleto},
-        cedula:${cedula},
-        grado:${grado},
-        seccion:${seccion},
-      })}
-  try {
-    const response = await fetch(url,requestOptions);
-    if (!response.ok) {
-      throw new Error(Response status: ${response.status});
-    }
+    },
+  };
 
-    const json = await response.json();
-    console.log(json);
+  const handledays = async (event, newdays) => {
+    console.log(newdays)
+    await setdays(newdays);
+  };
 
-
-      try {
-        const changemode = await fetch("http://192.168.1.106/off")
-        console.log(changemode)
-      } catch (error) {
-        console.log(error.message)
-      }
-
-    
-    
-  } catch (error) {
-    console.error(error.message);
-  }
-  }*/
-
-  const [Name,setName] = useState("")
-  const [Teacher,setTeacher] = useState("")
-  const [Seccion,setSeccion] = useState("")
 
   const handleTeacher = (event) => {
     setTeacher(event.target.value);
@@ -70,8 +65,15 @@ function NewGrade() {
 
   };
 
-  return(
+  const isWeekend = (date) => {
+    const day = date.day();
+  
+    return day === 0 || day === 6;
+  };
 
+
+
+  return(
 <>
 <Toolbar sx={{ backgroundColor: theme.palette.primary.light, marginBottom: '30px'}}>
       <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
@@ -84,14 +86,16 @@ function NewGrade() {
 
 <Box sx={{ width: '650px', backgroundColor: theme.palette.primary.light, marginLeft: 'auto',marginRight: 'auto', borderRadius: 1}}alignItems="center">
 
-    
 
     <Grid container spacing={2} sx={{ padding: '25px',}}>
       
-        <Grid item xs={12} >
+        <Grid style={{
+          display:'flex',
+          flexDirection:'column'
+        }} item xs={12} >
               <Grid container spacing={1}>
                   <Grid item xs={12}>
-                      <TextField sx={{width: '100%' }} id="outlined-basic" label="Name" variant="outlined" valor={Name} setvalor={setName} />
+                      <TextField sx={{width: '100%' }} id="outlined-basic" label="Grade Name" variant="outlined" valor={Name} setvalor={setName} />
                   </Grid>
 
                   <Grid item xs={6}>
@@ -111,7 +115,51 @@ function NewGrade() {
                                 
                        </Select>
                    </FormControl>
+                   
+                   <FormControl fullWidth style={{
+        marginTop:"8px",
+      }}>
+                       <InputLabel id="demo-simple-select-label">Weeks</InputLabel>
+                           <Select
+                             labelId="demo-simple-select-label"
+                             id="demo-simple-select"
+                             value={Weeks}
+                             label="Grado"
+                             onChange={(e)=>{setWeeks(e.target.value)}}
+                             MenuProps={MenuProps}
+                           >
+                                <MenuItem value={1}>1 Week</MenuItem>
+                                <MenuItem value={2}>2 Weeks</MenuItem>
+                                <MenuItem value={3}>3 Weeks</MenuItem>
+                                <MenuItem value={4}>4 Weeks</MenuItem>
+                                <MenuItem value={5}>5 Weeks</MenuItem>
+                                <MenuItem value={6}>6 Weeks</MenuItem>
+                                <MenuItem value={7}>7 Weeks</MenuItem>
+                                <MenuItem value={8}>8 Weeks</MenuItem>
+                                <MenuItem value={9}>9 Weeks</MenuItem>
+                                <MenuItem value={10}>10 Weeks</MenuItem>
+                                <MenuItem value={11}>11 Weeks</MenuItem>
+                                <MenuItem value={12}>12 Weeks</MenuItem>
+                                <MenuItem value={13}>13 Weeks</MenuItem>
+                                    
+                       </Select>
+                   </FormControl>
+
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['TimePicker']}>
+
+        <TimePicker value={TimeStart}
+        onChange={(newValue) => setTimeStart(newValue)}
+        label="Start time" 
+        slotProps={{ textField: { fullWidth:'true' }}}/>
+
+      </DemoContainer>
+    </LocalizationProvider>
+
+
                   </Grid>
+
+
                   <Grid item xs={6}>
                   <FormControl fullWidth>
                        <InputLabel id="demo-simple-select-label">Seccion</InputLabel>
@@ -127,14 +175,70 @@ function NewGrade() {
                                 <MenuItem value={30}>C</MenuItem>        
                        </Select>
                    </FormControl>
+                   
+                   <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DatePicker']}>
+        <DatePicker
+        disablePast={true}
+        shouldDisableDate={isWeekend}
+        value={startDate}
+        onChange={(newValue) => setstartDate(newValue)} 
+        label="Start Date" 
+        slotProps={{ textField: { fullWidth:'true' }}}/>
+      </DemoContainer>
+    </LocalizationProvider>
+
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['TimePicker']}>
+        <TimePicker  value={timeEnd}
+        onChange={(newValue) => setTimeEnd(newValue)}label="End time" slotProps={{ textField: { fullWidth:'true' }}}/>
+      </DemoContainer>
+    </LocalizationProvider>
+
+
                   </Grid>
  
               </Grid>
-        </Grid>
 
+              <h4 style={{color: theme.palette.primary.dark,marginLeft:'auto', marginRight:'auto',}}>Class days</h4>
+
+              <ToggleButtonGroup
+      value={days}
+      onChange={handledays}
+      aria-label="text formatting"
+      style={{
+        marginLeft:'auto',
+        marginRight:'auto',
+        position:"relative"
+      }}
+    >
+      <ToggleButton value="Sunday" aria-label="bold" disabled style={{minWidth:75,minHeight:75}}>
+      Sun
+      </ToggleButton>
+      <ToggleButton value="Monday" aria-label="bold" style={{minWidth:75,minHeight:75}}>
+      Mon
+      </ToggleButton>
+      <ToggleButton value="Tuesday" aria-label="bold" style={{minWidth:75,minHeight:75}}>
+      Tues
+      </ToggleButton>
+      <ToggleButton value="Wednesday" aria-label="bold" style={{minWidth:75,minHeight:75}}>
+      Wed
+      </ToggleButton>
+      <ToggleButton value="Thursday" aria-label="bold" style={{minWidth:75,minHeight:75}}>
+      Thurs
+      </ToggleButton>
+      <ToggleButton value="Friday" aria-label="bold" style={{minWidth:75,minHeight:75}}>
+      Fri
+      </ToggleButton>
+      <ToggleButton value="Saturday" aria-label="bold" disabled style={{minWidth:75,minHeight:75}}>
+      Sat
+      </ToggleButton>
+    </ToggleButtonGroup>
+        </Grid>
+        
 
     <div style={{margin: '20px', marginLeft: 'auto',marginRight: 'auto'}}>
-        <Button variant="contained">Continue</Button>
+        <Button onClick={()=>{console.log(days)}} variant="contained">Continue</Button>
     </div>
 </Grid>
 
