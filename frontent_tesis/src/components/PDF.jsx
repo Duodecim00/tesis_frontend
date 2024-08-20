e91e0748b796fd1a3d3bc6b84eddb721d8ff38dc
Duodecim00 <josem2740@gmail.com>
2024-08-20 15:20:23 -0400

    estilos del archivo pdf

--- frontent_tesis/src/components/PDF.jsx
+++ frontent_tesis/src/components/PDF.jsx
@@ -6,16 +6,17 @@ import { pilares } from '../data/data';
 const styles = StyleSheet.create({
 
     alumno:{
-        width:'80%',
+        width:'50%',
         marginTop:'20px',
         marginLeft:'auto',
         marginRight:'auto',
-        padding: '5px',
-        fontSize:'10px',     
+        fontSize:'10px',
+        border:'1px solic #000000',     
     },
     alumnodata:{
-        fontSize:'20px',
+        fontSize:'13px',
         padding:'5px',
+        borderBottom:'1px solic #000000',
     },
     page:{
         padding:'20px',
@@ -30,19 +31,19 @@ const styles = StyleSheet.create({
         flexDirection:'column',
         display:'flex',
         alignItems:'center',
-        padding:'10px'
+        padding:'16px'
     },
     section:{
-        width:'80%',
+        width:'100%',
         display:'flex',
-        justifyContent:'center',
-        alignContent:'flex-end',
+        justifyContent: 'center',
+        alignItems:'center',
         flexDirection:'column'
     },
     text:{
-        fontSize:'11px',
+        fontSize:'13px',
         padding:'5px',
-        textAlign:'center'
+        display:'flex',
     },
     table:{
         width:'80%',
@@ -58,38 +59,38 @@ const styles = StyleSheet.create({
     },
     row:{
         flexDirection:'row',
-        border:'1px solic #ccc',
+        border:'1px solic #000000',
     },
     header:{
         width:'15px',
         textAlign:'center',
         fontWeight:'800',
         textTransform:'uppercase',
-        color:'royalblue',
-        border:'1px solic #ccc'
+        border:'1px solic #000000'
     },
     header0:{
         width:'24px',
         textAlign:'center',
         fontWeight:'800',
         textTransform:'uppercase',
-        color:'royalblue',
-        border:'1px solic #ccc'
+        border:'1px solic #000000'
     },
     cell:{
         width:'15px',
         textAlign:'center',
-        color:'#222',
-        border:'1px solic #ccc',
+        color:'#000000',
+        border:'1px solic #000000',
     },
     cell0:{
         width:'24px',
         textAlign:'center',
-        color:'#222',
-        border:'1px solic #ccc',
+        color:' #000000',
+        border:'1px solic #000000',
     },
     containerText:{
-        width:'400px'
+        width:'500px',
+        marginLeft:'auto',
+        marginRight:'auto',
     }
 
 })
@@ -109,11 +110,14 @@ const Pdf = () => {
             <Document>
             <Page size={"A4"} style={styles.Page}>
                 <View style={styles.alumno}>
+                <View style={styles.alumnodata}>
+                        <Text>Date:XX-XX-XXXX</Text>
+                    </View>
                     <View style={styles.alumnodata}>
                         <Text>Name:XXXXXXX</Text>
                     </View>
                     <View style={styles.alumnodata}>
-                        <Text>Last:XXXXXXX</Text>
+                        <Text>Last Name:XXXXXXX</Text>
                     </View>
                     <View style={styles.alumnodata}>
                         <Text>CI:XXXXXXX</Text>
@@ -121,6 +125,17 @@ const Pdf = () => {
                     <View style={styles.alumnodata}>
                         <Text>Año:XXX   Seccion:XXX</Text>
                     </View>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.title}> registro de asistencia</Text>
+                </View>
+                    <View style={styles.containerText}>
+                        <Text style={styles.text}> a la fecha en que es emitido este documento el alumno 
+                         XXXXXX XXXXXXX es poseedor de una asitencia del X%
+                        </Text>
+                    </View>
+                <View style={styles.section}>
+                    <Text style={styles.title}> tabla de asistena</Text>
                 </View>
                 <View style={styles.table}>
                     <View style={styles.row}>
